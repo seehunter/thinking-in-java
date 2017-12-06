0c8c5f56ec548a42a142377f55bea79d80ecb31a
Lixg <2249975638@qq.com>
2017-12-06 10:10:49 +0800

    Demonstration of both constructor and ordinary method overloading

---
+++ src/main/java/book/thinginginjava/chapter05/c_Overloading.java
@@ -0,0 +1,37 @@
+package book.thinginginjava.chapter05;
+
+// Demonstration of both constructor and ordinary method overloading
+
+class Tree {
+    int height;
+
+    Tree() {
+        System.out.println("Planting a seedling");
+        height = 0;
+    }
+
+    Tree(int initialHeight) {
+        height = initialHeight;
+        System.out.println("Creating new tree that is " + height + " feet tall");
+    }
+
+    void info() {
+        System.out.println("Tree is " + height + " feet tall");
+    }
+
+    void info(String s) {
+        System.out.println(s + ": Tree is " + height + " feet tall");
+    }
+}
+
+public class c_Overloading {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Tree t = new Tree(i);
+            t.info();
+            t.info("overloaded method");
+        }
+        // Overload constructor
+        new Tree();
+    }
+}
