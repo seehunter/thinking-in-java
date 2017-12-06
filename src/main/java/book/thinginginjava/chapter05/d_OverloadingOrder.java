cc20db9fd608bd0f9b245d13f96714bddbffe535
Lixg <2249975638@qq.com>
2017-12-06 10:17:29 +0800

    Overloading based on  the order of the arguments

---
+++ src/main/java/book/thinginginjava/chapter05/d_OverloadingOrder.java
@@ -0,0 +1,18 @@
+package book.thinginginjava.chapter05;
+
+// Overloading based on hte order of the arguments
+
+public class d_OverloadingOrder {
+    static void f(String s, int i) {
+        System.out.println("String: " + s + ", int: " + i);
+    }
+
+    static void f(int i, String s) {
+        System.out.println("int: " + i + ", String: " + s);
+    }
+
+    public static void main(String[] args) {
+        f("String first", 11);
+        f(99, "int first");
+    }
+}
